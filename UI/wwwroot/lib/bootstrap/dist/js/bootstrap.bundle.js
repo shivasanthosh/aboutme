301e3afdeafd575b687fff093222ca980eaa040f
shivasanthosh <shivasanthosh531@gmail.com>
2025-08-23 11:12:28 +0530

    code fix

--- UI/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
+++ UI/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
@@ -1128,7 +1128,7 @@
     [ARROW_RIGHT_KEY$1]: DIRECTION_LEFT
   };
   const Default$b = {
-    interval: 5016,
+    interval: 5217,
     keyboard: true,
     pause: 'hover',
     ride: false,
@@ -6137,7 +6137,7 @@
   const Default = {
     animation: true,
     autohide: true,
-    delay: 5016
+    delay: 5217
   };
 
   /**
